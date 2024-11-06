24a798bcc682934cb7b0689222d9796798b1ff26
goeun <rhdms6748@gmail.com>
2024-11-06 17:27:54 +0900

    20241106 commit

--- src/Components/slidebanner.tsx
+++ src/Components/slidebanner.tsx
@@ -4,31 +4,46 @@ import React,{useState} from "react";
 
 const bannerItems:BannerItem [] =bannerImgs.map((imgSrc)=> ({
     src: imgSrc
-    }));
+}));
 
 const SlideBanner:React.FC = () => {
 
-    const [isSlide,setIsSlide] = useState(false);
-    const [currIndex,setCurrIndex] = useState(0);
+    const [currIndex, setCurrIndex] = useState(0);
+    //마우스 시작 위치 = startX
+    const [startX,setStartX] = useState(0);
+    //슬라이드 상태 확인
+    const [isSlide,setIsSlide] =useState(false);
 
-    const HandleSlide=()=>{
+    const prevIndex = (currIndex - 1 + bannerItems.length) % bannerItems.length;
+    const nextIndex = (currIndex + 1) % bannerItems.length;
+
+    //이벤트가 일어남 -> isSlide = true, 이벤트 시작 위치 -> screenX 로 지정
+    const StartEvent = (e:React.MouseEvent<HTMLDivElement>) =>{
         setIsSlide(!isSlide);
+        setStartX(e.screenX);
     }
 
-    const prevIndex = (currIndex - 1 + bannerItems.length)% bannerItems.length;
-    const nextIndex = (currIndex + 1 ) % bannerItems.length;
+    //슬라이드가 true 일때 상태 변화 감지 
 
-    const setNext=()=>{
-        setCurrIndex(nextIndex);
+    const MoveSlide = (e:React.MouseEvent<HTMLDivElement>) =>{
+
+        //마우스 이동 거리 = 이벤트 시작 위치 - 이벤트 종료 위치
+        const diffX = e.screenX - startX;
+
+        //마우스 이동 거리 > 80px 일때 , 이전 배너로 슬라이드    
+        //마우스 이동 거리 < -80px 일때 , 다음 배너로 슬라이드 
+
     }
 
-    const setPrev=()=>{
-        setCurrIndex(prevIndex);
+    //아무것도 전달 되지 않을때 = 이벤트 종료
+    const EndEvent = () =>{
+        setIsSlide(isSlide);
     }
+
 
     return(
             <div /*슬라이더가 보여지는 부분*/className="slide_banner_wrap">
-                <div /*배너 전체묶음 : x 축 기준으로 이동*/ onChange={HandleSlide} className="slide_banner_group">
+                <div /*배너 전체묶음 : x 축 기준으로 이동*/ className="slide_banner_group">
                     {bannerItems.map((item,currIndex)=>{
                         
                         return(
